4fd3f4aa7f0757b969bdd1213a50e1a0382619e7
Galacticus3 <Galacticus_life@mail.ru>
2014-06-25 13:54:18 +0300

    все окрім + - * /  =
    все окрім + - * /  =

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -12,9 +12,90 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        
+        
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        //If TextBox1.Text.Contains(",") Then Else : TextBox1.AppendText(",")
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains(",") == false) textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        // C
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        // sqrt
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(Math.Sqrt(Convert.ToDouble(textBox1.Text)));
+        }
+
+        // + -
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString((Convert.ToDouble(textBox1.Text))*(-1));
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString((1/Convert.ToDouble(textBox1.Text)));
         }
     }
 }
